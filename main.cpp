1bc1f2a5362c1e78db0b32f089667fe665cbcf51
Antony C <antony.coco@live.fr>
2023-09-21 20:25:05 +0200

    maj du main

--- main.cpp
+++ main.cpp
@@ -2,33 +2,44 @@
 #include <string>
 #include <thread>
 #include <chrono>
+#include <atomic>
+#include <csignal>
+#include <mutex>
 
 #include "include/connectApi/apiHandler.h" 
 #include "include/connectApi/concreteObserver.h"
 #include "include/service/periodicFetcher.h"
+
+std::atomic<bool> stopRequested(false); 
+std::mutex mtx;
+
+void signalHandler(int signum) {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::cout << "Signal reçu: " << signum << ". Arrêt demandé." << std::endl;
+    stopRequested.store(true);
+}
+
 int main(int argc, char **argv) {
+    std::signal(SIGINT, signalHandler);
 
     ConcreteObserver observerUnique;
-    // ConcreteObserver observerDaily;
+    ConcreteObserver observerDaily;
 
     APIHandler apiHandlerForUnique;
-    // APIHandler apiHandlerForDaily;
+    APIHandler apiHandlerForDaily;
 
     apiHandlerForUnique.addObserver(&observerUnique);
-    // apiHandlerForDaily.addObserver(&observerDaily);
+    apiHandlerForDaily.addObserver(&observerDaily);
 
-    // // Appel à l'API et notification des observateurs
+    // Appel à l'API et notification des observateurs
     apiHandlerForUnique.fetchData();
-    /* // abonnement journalier evenement
+    
     PeriodicFetcher fetcher(apiHandlerForDaily);
     std::thread fetchThread(&PeriodicFetcher::start, &fetcher);
-    // Pause pour tester plusieur requete pour tester (modifier aussi le sleep_for dans periodicFetcher.cpp)
-    //std::this_thread::sleep_for(std::chrono::seconds(10));
-    cout << "...Sleep over" << endl;
 
-    fetcher.stop();
+    std::cout << "Attente de l'arrêt du fetcher..." << std::endl;
     fetchThread.join();
-    */
+    std::cout << "Fetcher arrêté. Fin du programme." << std::endl;
 
     return 0;
 }
--- src/service/periodicFetcher.cpp
+++ src/service/periodicFetcher.cpp
@@ -1,12 +1,43 @@
 #include "../../include/service/periodicFetcher.h"
 
-void PeriodicFetcher::start()
+PeriodicFetcher* PeriodicFetcher::instance = nullptr;
+
+PeriodicFetcher::PeriodicFetcher(Observable& observable)
+    : observable(observable), stopRequested(false), pieChartGen()
 {
-    APIHandler apiHandler;
-    while (!stopFetching) {
-        std::map<std::string, nlohmann::json> newEvents = apiHandler.fetchTodaysEvents();
-        observable.notifyDailyObservers(newEvents);
-        pieChartGen.generatePieChart(newEvents);
-        std::this_thread::sleep_for(std::chrono::hours(1));
+    if (!instance) {
+        instance = this;
+        // Associer le signal SIGINT (CTRL+C) à notre gestionnaire de signal
+        std::signal(SIGINT, signalHandler);
+
     }
+}
+
+void PeriodicFetcher::signalHandler(int signum)
+{
+    std::cout << "[PeriodicFetcher] Signal d'arrêt reçu : " << signum << std::endl;
+    if (instance) {
+        instance->stopRequested.store(true);
+    }
+}
+
+void PeriodicFetcher::start() {
+    APIHandler apiHandler;
+    while (!stopRequested.load()) {
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            std::map<std::string, nlohmann::json> newEvents = apiHandler.fetchTodaysEvents();
+            observable.notifyDailyObservers(newEvents);
+            pieChartGen.generatePieChart(newEvents);
+        }
+
+        std::cout << "[PeriodicFetcher] Nouveaux événements récupérés et traités." << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::hours(1));
+
+        std::cout << "[PeriodicFetcher] Thread fetcher : encore en exécution." << std::endl;
+    }
+
+    std::cout << "[PeriodicFetcher] Arrêt en cours..." << std::endl;
+    std::cout << "[PeriodicFetcher] Thread fetcher : arrêté." << std::endl;
 }
